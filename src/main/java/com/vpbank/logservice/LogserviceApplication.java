4919c153adc40faef3b6adadb7e80e55d53589d3
extp-cmc-thohx <ext_cmc_thohx@vpbank.com.vn>
2022-01-06 13:47:27 +0700

    init logservice

---
+++ src/main/java/com/vpbank/logservice/LogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vpbank.logservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogserviceApplication.class, args);
+	}
+
+}
